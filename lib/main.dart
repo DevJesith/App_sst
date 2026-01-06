e9e3b48af22618c0603ad8e7a085020dd3d1d235
Jesith Mejia <jesithmanuel@gmail.com>
2026-01-06 12:38:46 -0500

    docs: Explanatory commentary on the dependencies used

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:app_sst/data/database/drop_clean_database.dart';
-import 'package:app_sst/features/auth/presentation/screens/check_auth_screen.dart';
+import 'package:app_sst/core/data/database/drop_clean_database.dart';
+import 'package:app_sst/features/auth/presentation/screens/startup/check_auth_screen.dart';
 import 'package:app_sst/services/connectivity_manager.dart';
 import 'package:app_sst/services/notification_service.dart';
 import 'package:flutter/material.dart';
